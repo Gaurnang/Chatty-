6fc979f08aa0518a9e4e1a5353b626e3897c9f54
Gaurang <gauranggoel250@gmail.com>
2026-07-06 17:35:54 +0530

    Dockerization

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
 import morgan from "morgan";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -13,8 +12,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
